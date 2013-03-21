f292b64bc5cf5776a5fdab789bc4ab680692fbf4
James <jrm.general@gmail.com>
2013-03-21 13:15:52 -0400

    Manual merge of HTML5 with multiple files

---
+++ HTML5_File_Api/js/app/main.js
@@ -0,0 +1,22 @@
+/*
+ * Author: James Meldrum
+ * Date: 3/20/2013
+ * Desc: Upload logic for labelwine. Kept as a simple script to avoid
+ *       overhead of a full javascript application. All application
+ *       functions are annonymous as this is the only external
+ *       script file for application logic, should still be debuggable
+ *       and maintainable.
+ */
+
+(function(){
+
+  "use strict";
+
+  if(window.File && window.FileReader && window.FileList && window.blob){
+
+  }else{
+    alert("The File API is not fully supported in this browser.");
+  }
+  
+}());
+
